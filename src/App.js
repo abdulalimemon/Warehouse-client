217e3db2643462a8c5106455173624fa6e2d873b
Emon02 <abdulalimemon11@gmail.com>
2022-10-21 23:17:27 +0600

    footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Navbar from './pages/Shared/Navbar';
+import Footer from './pages/Shared/Footer';
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
-
+      <Footer></Footer>
     </>
   );
 }
