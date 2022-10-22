1f875131b8a08107e72ee59ddcc893b7c92c5c05
Emon02 <abdulalimemon11@gmail.com>
2022-10-22 21:26:01 +0600

    Slider

--- src/pages/Home/HeroSection.js
+++ src/pages/Home/HeroSection.js
@@ -1,39 +1,36 @@
-// Import Swiper React components
-import React from 'react';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
-
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import Pic from '../../assets/pic.jpg';
-
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
+import './Home.css';
+import Slide from "./Slide";
 
 const HeroSection = () => {
-    return (
-        <>
-            <Swiper
-      // install Swiper modules
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={50}
-      slidesPerView={3}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      ...
-    </Swiper>
-        </>
-    );
+  return (
+    <>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <Slide></Slide>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Slide></Slide>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Slide></Slide>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
 };
 
 export default HeroSection;
